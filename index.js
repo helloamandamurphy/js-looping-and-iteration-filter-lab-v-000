2277f8055a26d9835de8b8efe1d48a2483b580ef
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-01 20:32:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function findMatching (drivers, name) { //creating a function that accepts an ar
 function fuzzyMatch(drivers, partialName) {
   //let nameLength = partialName.length
   return drivers.filter(function (partialMatch) {
-    return partialMatch() === name.slice(0, (partialName.length))
+    return 
   })
 }
 
