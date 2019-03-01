a62ef98d4a364df251edc9a4e0e96a09b1b2a906
Amanda Murphy <helloamandamurphy@gmail.com>
2019-03-01 20:19:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching (drivers, name) {
-  const matchingDrivers = drivers.filter(name.lowercase);
+  const matchingDrivers = drivers.filter(name.toLowerCase);
 
 console.log(result);
 }
